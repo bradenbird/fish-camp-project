552468f2bef665e4a2f0bacdc6f53a051d937d6b
James Vanderburg <james@vanderburg.org>
2018-03-05 23:54:21 -0600

    add applicant_id to session_availabilities

--- db/migrate/20180305233739_create_session_availabilities.rb
+++ db/migrate/20180305233739_create_session_availabilities.rb
@@ -2,7 +2,7 @@ class CreateSessionAvailabilities < ActiveRecord::Migration[5.1]
   def change
     create_table :session_availabilities do |t|
       t.references :session, null: false
-      # TODO: add t.references :application later
+      t.references :applicant, null: false
       t.timestamps null: false
     end
   end
