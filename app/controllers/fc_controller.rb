bf8ea014f70054304833780a9ec93975a6ebedfc
stonengineer <josephwstone@gmail.com>
2018-03-06 11:00:36 -0600

    Added login redirect page, trying to set up auto redirect to register/profile page if logged in

--- app/controllers/fc_controller.rb
+++ app/controllers/fc_controller.rb
@@ -16,6 +16,17 @@ class FcController < ApplicationController
     #do nothing yet
   end
 
+  def login
+    requested_path = params[:reqpath]
+    if current_user then
+      if requested_path then
+        redirect_to(requested_path)
+      else
+        puts("Error")
+      end
+    end
+  end
+
   def show
     redirect_to home_path
   end
