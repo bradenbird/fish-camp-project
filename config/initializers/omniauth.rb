7c66de017205c7e3119691992b8f73a70111825f
Braden Bird <bradenbird@tamu.edu>
2018-03-06 18:39:24 -0600

    Added omniauth.rb that uses environment variables

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,8 @@
+# Do not use keys in plain text on GitHub
+
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+	#Set environment variables for usage
+	provider :google_oauth2, ENV['GOOGLE_CLIENT'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
+end
