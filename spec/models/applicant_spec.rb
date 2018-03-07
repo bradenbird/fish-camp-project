81186d0e471e7db73d64f3116e3fee8b423b4fa8
James Vanderburg <james@vanderburg.org>
2018-03-06 21:18:00 -0600

    Forgot to change Gemfile

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Applicant, type: :model do
-  it "has a valid factory"
+  it "has a valid factory" do
+    Factory.create(:applicant).should be_valid
+  end
   it "is invalid without a submission_id"
   it "is invalid without a uin"
 end
