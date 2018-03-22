aac1d8ffc963ab237daa4e01ebdbf48562b93a26
stonengineer <josephwstone@gmail.com>
2018-03-22 16:34:14 -0500

    extended homepage changes to registration page

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	#Set environment variables for usage
-	provider :google_oauth2, ENV['GOOGLE_CLIENT'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
+	#! ! ! CHANGE ! ! !
+	provider :google_oauth2, '506434569924-4ago21nj9cjdd6t1vjco41lbch8daqbn.apps.googleusercontent.com', 'ONLPQqgdxODQlkDy_k2mRNZU', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
 end
