420dbc2ac051c0deca8ff6b01aa4538ba477d7bc
James Vanderburg <james@vanderburg.org>
2018-03-24 18:34:46 -0500

    Test passing for <chair>.applicants

--- spec/models/chair_spec.rb
+++ spec/models/chair_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Chair, type: :model do
+  # let(:sessiona) { create(:session, name: "A") }
+
   it "has a valid factory" do
     expect(create(:chair)).to be_valid
   end
@@ -9,5 +11,21 @@ RSpec.describe Chair, type: :model do
   end
   it "is invalid without a user" do
     expect(build(:chair, user: nil)).to_not be_valid
+  end
+
+  describe "#applicants" do
+    it "gets all applicants for the chair's session" do
+      sessiona = create(:session, name: "A")
+      sessionb = create(:session, name: "B")
+      chair = create(:chair, camp: create(:camp, session: sessiona))
+      applicant1 = create(:applicant)
+      applicant1.session_availabilities.create(session: sessiona)
+      applicant2 = create(:applicant)
+      applicant2.session_availabilities.create(session: sessiona)
+      applicant2.session_availabilities.create(session: sessionb)
+      applicant3 = create(:applicant)
+      applicant3.session_availabilities.create(session: sessionb)
+      expect(chair.applicants).to contain_exactly(applicant1, applicant2)
+    end
   end
 end
