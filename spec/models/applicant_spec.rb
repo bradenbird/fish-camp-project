18bf699ac6ffca1d4d90611b6d9b1cab6055b359
James Vanderburg <james@vanderburg.org>
2018-03-25 16:55:01 -0500

    Added tests for creating the proper session_availabilites, and added testing of the second row

--- spec/models/applicant_spec.rb
+++ spec/models/applicant_spec.rb
@@ -23,12 +23,21 @@ RSpec.describe Applicant, type: :model do
       file_path = "#{Rails.root}/spec/fixtures/fc_sample.csv"
       Applicant.import(file_path, ".csv")
     end
+    let(:applicant1) { Applicant.find_by(submission_id: 12255241) }
+    let(:applicant2) { Applicant.find_by(submission_id: 12389115) }
     it "imports first names properly" do
-      # byebug
-      expect(Applicant.find_by(submission_id: 12255241).first_name).to eq("Brooke")
+      expect(applicant1.first_name).to eq("Brooke")
+      expect(applicant2.first_name).to eq("Corey")
     end
     it "imports last names properly" do
-      expect(Applicant.find_by(submission_id: 12255241).last_name).to eq("Aaron")
+      expect(applicant1.last_name).to eq("Aaron")
+      expect(applicant2.last_name).to eq("Able")
+    end
+    it "creates the proper session_availabilities" do
+      session_names1 = applicant1.sessions.map{|s| s.name}
+      session_names2 = applicant2.sessions.map{|s| s.name}
+      expect(session_names1).to contain_exactly("A", "B", "C", "D", "E", "F", "G")
+      expect(session_names2).to contain_exactly("B", "C", "D", "E", "F", "G")
     end
   end
 end
