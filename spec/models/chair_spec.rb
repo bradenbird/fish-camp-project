22d568b36cfcb8be699b55d549260a4cbb8771d3
James Vanderburg <james@vanderburg.org>
2018-03-24 21:03:55 -0500

    Passing test for <chair>.unevaluated_applicants

--- spec/models/chair_spec.rb
+++ spec/models/chair_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Chair, type: :model do
-  # let(:sessiona) { create(:session, name: "A") }
+  let(:sessiona) { create(:session, name: "A") }
+  let(:sessionb) { create(:session, name: "B") }
+  let(:applicant1) { create(:applicant) }
+  let(:applicant2) { create(:applicant) }
+  let(:applicant3) { create(:applicant) }
 
   it "has a valid factory" do
     expect(create(:chair)).to be_valid
@@ -15,17 +19,24 @@ RSpec.describe Chair, type: :model do
 
   describe "#applicants" do
     it "gets all applicants for the chair's session" do
-      sessiona = create(:session, name: "A")
-      sessionb = create(:session, name: "B")
       chair = create(:chair, camp: create(:camp, session: sessiona))
-      applicant1 = create(:applicant)
       applicant1.session_availabilities.create(session: sessiona)
-      applicant2 = create(:applicant)
       applicant2.session_availabilities.create(session: sessiona)
       applicant2.session_availabilities.create(session: sessionb)
-      applicant3 = create(:applicant)
       applicant3.session_availabilities.create(session: sessionb)
       expect(chair.applicants).to contain_exactly(applicant1, applicant2)
+    end
+  end
+
+  describe "#unevaluated_applicants" do
+    it "gets all applicants for the chair's session that the chair has not yet evaluated" do
+      chair = create(:chair, camp: create(:camp, session: sessiona))
+      applicant1.session_availabilities.create(session: sessiona)
+      applicant2.session_availabilities.create(session: sessiona)
+      applicant2.session_availabilities.create(session: sessionb)
+      applicant3.session_availabilities.create(session: sessionb)
+      chair.evaluations.create!(applicant: applicant1, rating: '9')
+      expect(chair.unevaluated_applicants).to contain_exactly(applicant2)
     end
   end
 end
