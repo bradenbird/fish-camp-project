aeed273930291d8de2e5dea6aa503056e5e82c7c
Braden Bird <bradencbird@gmail.com>
2018-03-25 15:31:50 -0500

    Fixed omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
 	#! ! ! CHANGE ! ! !
-	provider :google_oauth2, '506434569924-4ago21nj9cjdd6t1vjco41lbch8daqbn.apps.googleusercontent.com', 'ONLPQqgdxODQlkDy_k2mRNZU', {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
+	provider :google_oauth2, ENV['GOOGLE_CLIENT'], ENV['GOOGLE_SECRET'], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
 end
