36bc5b8e0dac82b013b04fa22349051a50536652
bradenbird <bradencbird@gmail.com>
2018-03-27 17:56:50 -0500

    Added checkbox for showing evaluated applicants or not

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,10 +1,19 @@
 class ApplicantsController < ApplicationController
   def index
     @applicants = Applicant.all
+    
     if params[:sessions].present?
       @current_sessions = params[:sessions].keys
     else
       @current_sessions = ("A".."G")
+    end
+
+    if params[:evaluated].present?
+      @show_evaluated = true
+      #Set applicants to show all
+    else
+      @show_evaluated = false
+      #Set applicants to show unevaluated
     end
   end
 
