3d481d24376a882a9a3f235c5499d0aa93ef6ec6
Braden Bird <bradencbird@gmail.com>
2018-04-06 12:05:18 -0500

    Implemented sorting by applicant id

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,12 +1,12 @@
 class ApplicantsController < ApplicationController
   def index
     @applicants = Applicant.all
-    
+
     if params[:sessions].present?
       @current_sessions = params[:sessions].keys
       @applicants = Applicant.joins(:sessions).where(sessions: {name: @current_sessions}).distinct
     else
-      @current_sessions = Session.all_session_names 
+      @current_sessions = Session.all_session_names
     end
 
     if params[:evaluated].present?
@@ -16,6 +16,26 @@ class ApplicantsController < ApplicationController
       @show_evaluated = false
       #Set applicants to show unevaluated
     end
+
+    @even_applicants, @odd_applicants = @applicants.partition{ |r| r.id.even? }
+    if params[:show].present?
+      @odd = false
+      @even = false
+      @all = false
+      if params[:show] == "odd"
+        @applicants = @odd_applicants
+        @odd = true
+
+      elsif params[:show] == "even"
+        @applicants = @even_applicants
+        @even = true
+      else
+        @all = true
+      end
+    else
+      @all = true
+    end
+
   end
 
   def show
@@ -25,12 +45,12 @@ class ApplicantsController < ApplicationController
 
   def import
     authorize Applicant, :create?
-    if params[:file].nil? 
+    if params[:file].nil?
       raise IOError, "Please Choose a file"
-    else 
+    else
       extension = File.extname(params[:file].original_filename)
       Applicant.import(params[:file].path, extension)
       redirect_to request.referrer, notice: "Applicants imported."
-    end 
+    end
   end
 end
