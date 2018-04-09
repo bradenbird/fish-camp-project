7cbbce00d83ad8f1c1fa29768aa363e30a4bab30
stonengineer <josephwstone@gmail.com>
2018-04-09 11:54:31 -0500

    updated bar links in Applicant Database tab

--- db/schema.rb
+++ db/schema.rb
@@ -111,8 +111,8 @@ ActiveRecord::Schema.define(version: 20180404185623) do
   end
 
   create_table "evaluations", force: :cascade do |t|
-    t.integer "chair_id"
-    t.integer "applicant_id"
+    t.integer "chair_id", null: false
+    t.integer "applicant_id", null: false
     t.string "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
