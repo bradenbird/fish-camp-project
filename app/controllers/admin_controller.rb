76bc11f67b36bdd53567ad4cef233ddeb5463883
t-lamp11 <tyler.s.lamkin@gmail.com>
2018-04-10 05:32:00 +0000

    Committing so I can check another branch

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,9 +5,15 @@ class AdminController < ApplicationController
     if params[:search1]
       @users = User.search(params[:search1]).order("created_at DESC").limit(25)
       @applicants = Applicant.all.order("created_at DESC").limit(25)
+      
     elsif params[:search2]
       @users = User.all.order("created_at DESC").limit(25)
       @applicants = Applicant.search(params[:search2]).order("created_at DESC").limit(25)
+      
+    elsif params[:search1] && params[:search2]
+      @users = User.search(params[:search1]).order("created_at DESC").limit(25)
+      @applicants = Applicant.search(params[:search2]).order("created_at DESC").limit(25)
+    
     else 
       @users = User.all.order("created_at DESC").limit(25)
       @applicants = Applicant.all.order("created_at DESC").limit(25)
