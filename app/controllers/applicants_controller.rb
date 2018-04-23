9ec5a9f9e49d084b19c281e31d1099fa2865299e
t-lamp11 <tyler.s.lamkin@gmail.com>
2018-04-23 21:21:35 +0000

    Finished the Applicant Edit Page/Update Function

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -82,7 +82,6 @@ class ApplicantsController < ApplicationController
     authorize Applicant, :create?
     uin = params[:uin]
     @applicant = Applicant.all.find_by 'uin = ?', uin
-    
     @applicant.first_name = params['applicant'][:first_name]
     @applicant.last_name = params['applicant'][:last_name]
     @applicant.uin = params['applicant'][:uin]
@@ -90,11 +89,60 @@ class ApplicantsController < ApplicationController
     @applicant.tamu_email = params['applicant'][:tamu_email]
     @applicant.other_email = params['applicant'][:other_email]
     @applicant.phone = params['applicant'][:phone]
+    #FC Info
     @applicant.session_availabilities.destroy_all
     params[:sessions].each do |session|
       curr_sess = Session.find_by(name: session)
       @applicant.session_availabilities.create!(session: curr_sess)
     end
+    @applicant.camp_counselor = params['applicant'][:camp_counselor]
+    @applicant.crew_counselor = params['applicant'][:crew_counselor]
+    @applicant.pick_up_only = params['applicant'][:pick_up_only]
+    @applicant.camp_history = params['applicant'][:camp_history]
+    @applicant.no_show_explanation = params['applicant'][:no_show_explanation]
+    #Parent Info
+    @applicant.parent_name = params['applicant'][:parent_name]
+    @applicant.parent_email = params['applicant'][:parent_email]
+    @applicant.parent_address = params['applicant'][:parent_address]
+    @applicant.parent_city = params['applicant'][:parent_city]
+    @applicant.parent_state = params['applicant'][:parent_state]
+    @applicant.parent_zip = params['applicant'][:parent_zip]
+    #Alternate Contact 1 Info
+    @applicant.alt_contact_1_name = params['applicant'][:alt_contact_1_name]
+    @applicant.alt_contact_1_email = params['applicant'][:alt_contact_1_email]
+    @applicant.alt_contact_1_address = params['applicant'][:alt_contact_1_address]
+    @applicant.alt_contact_1_city = params['applicant'][:alt_contact_1_city]
+    @applicant.alt_contact_1_state = params['applicant'][:alt_contact_1_state]
+    @applicant.alt_contact_1_zip = params['applicant'][:alt_contact_1_zip]
+    #Alternate Contact 2 Info
+    @applicant.alt_contact_2_name = params['applicant'][:alt_contact_2_name]
+    @applicant.alt_contact_2_email = params['applicant'][:alt_contact_2_email]
+    @applicant.alt_contact_2_address = params['applicant'][:alt_contact_2_address]
+    @applicant.alt_contact_2_city = params['applicant'][:alt_contact_2_city]
+    @applicant.alt_contact_2_state = params['applicant'][:alt_contact_2_state]
+    @applicant.alt_contact_2_zip = params['applicant'][:alt_contact_2_zip]
+    #Insurance Info
+    @applicant.insurance_provider = params['applicant'][:insurance_provider]
+    @applicant.insurance_policy_number = params['applicant'][:insurance_policy_number]
+    @applicant.insurance_policy_holder_name = params['applicant'][:insurance_policy_holder_name]
+    #Health Info
+    @applicant.last_tetanus_booster_date = params['applicant'][:last_tetanus_booster_date]
+    @applicant.drug_allergies = params['applicant'][:drug_allergies]
+    @applicant.food_allergies = params['applicant'][:food_allergies]
+    @applicant.dietary_red_meat = params['applicant'][:dietary_red_meat]
+    @applicant.dietary_vegan = params['applicant'][:dietary_vegan]
+    @applicant.dietary_vegetarian = params['applicant'][:dietary_vegetarian]
+    @applicant.dietary_dairy_free = params['applicant'][:dietary_dairy_free]
+    @applicant.dietary_gluten_free = params['applicant'][:dietary_gluten_free]
+    @applicant.dietary_other = params['applicant'][:dietary_other]
+    @applicant.dietary_none = !@applicant.dietary_red_meat && !@applicant.dietary_vegan && !@applicant.dietary_vegetarian && !@applicant.dietary_dairy_free && !@applicant.dietary_gluten_free && @applicant.dietary_other.blank?
+    @applicant.medications = params['applicant'][:medications]
+    @applicant.accommodations_auditory = params['applicant'][:accommodations_auditory]
+    @applicant.accommodations_visual = params['applicant'][:accommodations_visual]
+    @applicant.accommodations_physical = params['applicant'][:accommodations_physical]
+    @applicant.accommodations_other = params['applicant'][:accommodations_other]
+    @applicant.accommodations_none = !@applicant.accommodations_auditory && !@applicant.accommodations_visual && !@applicant.accommodations_physical && @applicant.accommodations_other.blank?
+    @applicant.other_medical_concerns = params['applicant'][:other_medical_concerns]
     @applicant.save!
     flash[:notice] = "You have updated the application"
     redirect_to "/applicants/edit?uin=#{@applicant.uin}"
