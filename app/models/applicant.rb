f1e0e39cf7f2269c3b0d39a583c21197cf65af45
James Vanderburg <james@vanderburg.org>
2018-04-24 17:32:48 -0500

    Passing rspec tests for create_chair

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -227,13 +227,6 @@ class Applicant < ApplicationRecord
   def self.open_spreadsheet(path, extension)
     raise IOError, "Please Choose a .csv, .xlx, or .xlsx file" unless (extension == ".csv" || extension == ".xls" || extension == ".xlsx")
     Roo::Spreadsheet.open(path, extension: extension)
-    # puts file.class.inspect
-    # case File.extname(file.original_filename)
-    # when ".csv" then Roo::CSV.new(file.path)
-    # when ".xls" then Roo::Excel.new(file.path)
-    # when ".xlsx" then Roo::Excelx.new(file.path)
-    # else raise "Unknown file type: #{file.original_filename}"
-    # end
   end
 
   def self.all_session_names
