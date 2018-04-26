bcc895e1db3cc9c74e5c9a7b9aa6d96efb7da876
James Vanderburg <james@vanderburg.org>
2018-04-26 15:06:06 -0500

    Added pagination to applicants in admin console

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -10,16 +10,17 @@ class AdminController < ApplicationController
     @users = User.all
     @applicants = Applicant.all
     if params[:search1]
-      @users = User.search(params[:search1]).order("created_at DESC").limit(50)
+      @users = User.search(params[:search1]).order("created_at DESC")
     else 
-      @users = User.all.order("created_at DESC").limit(50)
+      @users = User.all.order("created_at DESC")
     end
     
     if params[:search2]
-      @applicants = Applicant.search(params[:search2]).order("created_at DESC").limit(50)
+      @applicants = Applicant.search(params[:search2]).order("created_at DESC")
     else 
-      @applicants = Applicant.all.order("created_at DESC").limit(50)
+      @applicants = Applicant.all.order("created_at DESC")
     end
+    @applicants = @applicants.paginate(page: params[:page], per_page: 20)
   end
 
   def show
