4d13e05794bf226c9d1a160e6d6409218c47b1d9
James Vanderburg <james@vanderburg.org>
2018-07-13 08:38:03 -0400

    Remove database cleaners

--- spec/support/features.rb
+++ spec/support/features.rb
@@ -18,14 +18,6 @@ RSpec.configure do |config|
   config.before(:suite) do
     Capybara.run_server = true
     Capybara.javascript_driver = :headless_chrome
-    # DatabaseCleaner.strategy = :truncation
   end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
-  #
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
 end
