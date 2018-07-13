9f6c90ceb04e5f87d1e9e9310061cb39aff79e9f
James Vanderburg <james@vanderburg.org>
2018-07-12 22:36:16 -0400

    Configure headless_chrome for Capybara tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,28 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 require 'support/factory_bot'
+
+require "selenium/webdriver"
+
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: { args: %w(headless disable-gpu) }
+  )
+
+  Capybara::Selenium::Driver.new app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+end
+
+# This line sets Capybara to use headless_chrome for javascript tests by default.
+# This is commented out now since there is no js frontend.
+# Capybara.javascript_driver = :headless_chrome
+
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
