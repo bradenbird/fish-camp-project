9a593f07eb5f7475ac1a9f8b553fbef73b06f011
James Vanderburg <james@vanderburg.org>
2018-07-13 09:09:55 -0400

    Add skeleton tests

--- spec/features/manage_admin_spec.rb
+++ spec/features/manage_admin_spec.rb
@@ -20,4 +20,52 @@ RSpec.feature "ManageAdmin", type: :feature do
     expect(page).to have_selector("h", text: "Fish Camp Admin Console")
   end
 
+  xit "it makes a user a guest" do
+
+  end
+
+  xit "makes a user a chair" do
+
+  end
+
+  xit "makes a user an admin" do
+
+  end
+
+  xit "accesses the update applicant page" do
+
+  end
+
+  xit "updates applicant fields" do
+
+  end
+
+  xit "deletes an applicant" do
+
+  end
+
+  xit "searches for applicants by name" do
+
+  end
+
+  xit "searches for users by name" do
+
+  end
+
+  xit "searches for applicants by UIN" do
+
+  end
+
+  xit "searches for users by email" do
+
+  end
+
+  xit "searches for users by role" do
+
+  end
+
+  xit "navigates to new pages" do
+    
+  end
+
 end
