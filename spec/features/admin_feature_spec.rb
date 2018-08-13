19d64ffa55a12cb9b51f059dc937c94a75ca86c6
James Vanderburg <james@vanderburg.org>
2018-08-12 22:30:17 -0400

    Add specs for changing user roles and updating applicants

--- spec/features/admin_feature_spec.rb
+++ spec/features/admin_feature_spec.rb
@@ -2,47 +2,62 @@ require 'rails_helper'
 
 RSpec.feature "AdminFeatures", type: :feature do
 
-  def setup
-    # TODO: seed with some static data to make tests more consistent
-    load "#{Rails.root}/db/seeds.rb"
-  end
-
-  # setup
-
-  context "when logged in as an admin" do
-
-    # before(:each) do
-    #   home_page.admin_sign_in
-    #   expect(page).to have_selector("p", text: "Signed in as Admin!")
-    #   admin_page.visit_page
-    #   expect(page).to have_selector("h", text: "Fish Camp Admin Console")
-    # end
-
+  context "when signing in" do
     it "signs in" do
       home_page.admin_sign_in
       expect(page).to have_selector("p", text: "Signed in as Admin!")
       admin_page.visit_page
       expect(page).to have_selector("h", text: "Fish Camp Admin Console")
     end
+  end
 
-    xit "it makes a user a guest" do
+  context "when logged in as an admin" do
 
+    before(:each) do
+      home_page.admin_sign_in
+      expect(page).to have_selector("p", text: "Signed in as Admin!")
+      admin_page.visit_page
+      expect(page).to have_selector("h", text: "Fish Camp Admin Console")
     end
 
-    xit "makes a user a chair" do
-
+    it "it makes a user a guest" do
+      other_user = create(:user, role: "admin")
+      admin_page.visit_page
+      admin_page.make_other_user_a_guest(other_user)
+      expect(other_user.reload.role).to eq("guest")
     end
 
-    xit "makes a user an admin" do
-
+    it "makes a user a chair" do
+      other_user = create(:user, role: "admin")
+      admin_page.visit_page
+      admin_page.make_other_user_a_chair(other_user)
+      other_user.reload
+      expect(other_user.role).to eq("chair")
+      expect(other_user.chair.camp.name).to eq("aqua")
+      expect(Session.find(other_user.chair.camp.session_id).name).to eq("E")
     end
 
-    xit "accesses the update applicant page" do
+    it "makes a user an admin" do
+      other_user = create(:user, role: "guest")
+      admin_page.visit_page
+      admin_page.make_other_user_an_admin(other_user)
+      expect(other_user.reload.role).to eq("admin")
+    end
 
+    it "accesses the update applicant page" do
+      new_applicant = create(:applicant)
+      admin_page.visit_page
+      admin_page.visit_applicant_update_page(new_applicant)
+      expect(page).to have_selector("h", text: "Fish Camp Applicant Update Page")
     end
 
     xit "updates applicant fields" do
-
+      new_applicant = create(:applicant)
+      admin_page.visit_page
+      admin_page.update_applicant(new_applicant)
+      new_applicant.reload
+      expect(new_applicant.first_name).to eq("James")
+      expect(new_applicant.last_name).to eq("Vanderburg")
     end
 
     xit "deletes an applicant" do
--- spec/support/pages/admin_page.rb
+++ spec/support/pages/admin_page.rb
@@ -2,7 +2,34 @@ class AdminPage < Page
   include Capybara::DSL
 
   def visit_page
-    visit "admin/index"
+    visit "/admin/index"
+  end
+
+  def make_other_user_a_guest(user)
+    find("tr#user_#{user.id}").click_link('Make Guest')
+  end
+
+  def make_other_user_a_chair(user)
+    find("tr#user_#{user.id}").click_link('Make Chair')
+    select 'E', :from => 'session_id'
+    select 'Aqua', :from => 'color'
+    find('input[type="submit"]').click
+  end
+
+  def make_other_user_an_admin(user)
+    find("tr#user_#{user.id}").click_link('Make Admin')
+  end
+
+  def visit_applicant_update_page(applicant)
+    find("tr#applicant_#{applicant.id}").click_link("#{applicant.uin}")
   end
   
+  def update_applicant(applicant)
+    # byebug
+    find("tr#applicant_#{applicant.id}").click_link("#{applicant.uin}")
+    fill_in 'applicant[first_name]', :with => 'James'
+    fill_in 'applicant[last_name]', :with => 'Vanderburg'
+    find('input[type="submit"]').click
+  end
+
 end
