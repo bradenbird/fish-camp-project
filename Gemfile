ab9f9f3ab3f8eec648aa0e6b96eff025548d77f5
Braden Bird <bradencbird@gmail.com>
2022-03-10 00:28:55 -0600

    Changes to provide secrets with .env and dotenv gem
    Making it further in auth, but complete process still
    not working. Opened an issue to tackle it further

--- Gemfile
+++ Gemfile
@@ -47,6 +47,8 @@ gem "will_paginate"
 
 gem 'bootsnap', require: false
 
+gem 'dotenv-rails', groups: [:development, :test]
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   # ! ! ! CHANGE ! ! !
-  provider :google_oauth2, ENV["GOOGLE_CLIENT"], ENV["GOOGLE_SECRET"], {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
+  provider :google_oauth2, ENV.fetch('GOOGLE_CLIENT'), ENV.fetch('GOOGLE_SECRET'), {client_options: {ssl: {ca_file: Rails.root.join("cacert.pem").to_s}}, skip_jwt: true}
 end
